d3191a511871b46de7841ca87a95d0cabfa3d555
ieva broberg <ieva.broberg@mazhr.com>
2023-08-27 10:21:18 +0300

    first commit + nav completed

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import { Container, Row } from "react-bootstrap"
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col>
+
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
